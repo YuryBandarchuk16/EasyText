7d7c969163b353e93d8b92b7a884c44c8bd4e493
YuryBandarchuk16 <yury.upwork.bandarchuk@gmail.com>
2016-08-17 13:07:52 +0300

    Built Main Menu

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,26 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    @FXML
+    private Button exitButton, addSynonymsButton, modifyTheTextButton;
+
+    @FXML
+    private void exitApplication() {
+        Main.close();
+    }
+
+    @FXML
+    private void openSynonymsWindow() {
+
+    }
+
+    @FXML
+    private void openTextModifyingWindow() {
+        Main.runTextModifyingWindow();
+    }
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,7 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -8,12 +9,43 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private static Stage primaryStage;
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+        Parent root = FXMLLoader.load(getClass().getResource("mainMenu.fxml"));
+        primaryStage.setTitle("Main menu");
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
+        primaryStage.setOnCloseRequest(e -> Platform.exit());
+        /*Controller controller = new Controller();
+        controller.exitButton = new Button("exitButton");
+        int nClicks = 0;
+        controller.exitButton.setOnAction(event -> {
+            System.out.println("Clicked " + 0 + " times.");
+        });
+        if (controller.exitButton == null) {
+            System.out.println("NULL!");
+        } else {
+            System.out.println("Everything is ok!");
+        }*/
+    }
+
+    public static void close() {
+        primaryStage.close();
+    }
+
+    public static void runTextModifyingWindow() {
+        Platform.runLater(new Runnable() {
+            public void run() {
+                try {
+                    new TextModifyingWindow().start(new Stage());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
 
